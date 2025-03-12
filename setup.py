6c5171eae8d1d22030b1c4dd60ba932ea620fbd1
Pawan Uikey <pawanuikey690@gmail.com>
2025-03-12 13:08:14 +0530

    Initital Set-up is done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Pawan Kumar Uikey",
+    author_email="pawanuikey690@gmail.com",
+    packages=find_packages()
+)
